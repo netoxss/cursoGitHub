2a2dae420625660db2e6903c338e42d52201c7c4
netoxss <netop0227@gmail.com>
2024-02-01 10:28:10 -0400

    Versão Final

---
+++ desafioAlura/Program.cs
@@ -0,0 +1,15 @@
+﻿void TituloOpcao(string titulo)
+{
+    int tamanho = titulo.Length;
+    string asterisco = "";
+
+    for (int i = 0; i < titulo.Length; i++)
+    {
+        asterisco = String.Empty.PadLeft(tamanho, '*');
+    }
+    Console.WriteLine(asterisco);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asterisco);
+}
+
+TituloOpcao("teste");
